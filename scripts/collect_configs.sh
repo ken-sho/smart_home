a2f063f9d89bb2d29bb36f30b5680f92593d0f0b
ken-sho <gakto1981@gmail.com>
2026-06-03 03:00:01 +0000

    backup: 03062026_030001

--- scripts/collect_configs.sh
+++ scripts/collect_configs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Собирает конфиги для Git
 
+source /opt/smart-home/.env
 REPO=/opt/smart-home-git
 
 echo "Collecting configs..."
@@ -10,6 +11,12 @@ cp /opt/smart-home/docker-compose.yml $REPO/
 
 # Prometheus
 cp /opt/smart-home/config/prometheus.yml $REPO/config/
+
+# Nginx
+cp /opt/smart-home/config/nginx/nginx.conf $REPO/config/nginx.conf
+
+# Homer
+cp /data/homer/config.yml $REPO/config/homer.yml
 
 # Network
 cp /etc/netplan/50-cloud-init.yaml $REPO/config/netplan.yaml
@@ -22,16 +29,16 @@ cp /data/homeassistant/configuration.yaml $REPO/config/ha_configuration.yaml
 cp /data/homeassistant/automations.yaml $REPO/config/ha_automations.yaml
 cp /data/homeassistant/scripts.yaml $REPO/config/ha_scripts.yaml
 
-echo "Done! Run git add/commit/push manually or use backup.sh"
-
 # Grafana dashboards
 echo "Exporting Grafana dashboards..."
 mkdir -p $REPO/config/grafana_dashboards
-curl -s http://admin:asd123@localhost:3000/api/search?type=dash-db | \
+curl -s http://admin:${GRAFANA_PASSWORD}@localhost:3000/api/search?type=dash-db | \
   python3 -c "import sys,json; [print(d['uid'],d['title']) for d in json.load(sys.stdin)]" | \
   while read uid title; do
-    curl -s "http://admin:asd123@localhost:3000/api/dashboards/uid/$uid" | \
+    curl -s "http://admin:${GRAFANA_PASSWORD}@localhost:3000/api/dashboards/uid/$uid" | \
       python3 -c "import sys,json; d=json.load(sys.stdin); print(json.dumps(d['dashboard'],indent=2))" \
       > "$REPO/config/grafana_dashboards/${uid}.json"
     echo "  Exported: $title"
   done
+
+echo "Done!"
